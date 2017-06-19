f432efa91e50021092ece37d7833f653d8bacb6f
王泽华 <zehua.wang@bl.com>
2017-06-19 10:46:49 +0800

    update

--- spider/cat/main.py
+++ spider/cat/main.py
@@ -1,6 +1,11 @@
-import requests
+"""
+A test spider.
+"""
+# pylint: disable=invalid-name
+# pylint: disable=line-too-long
 import re
 import os
+import requests
 
 header = {
     'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36',
@@ -9,81 +14,88 @@ header = {
 }
 
 def login():
-    url = 'https://accounts.douban.com/login'
+    """login"""
+    # url = 'https://accounts.douban.com/login'
 
 def getPage(r):
-    reg1 = re.compile('<div.*?class="paginator".*?>(.*?)</div>',re.S|re.M)
+    """get all page links"""
+    reg1 = re.compile('<div.*?class="paginator".*?>(.*?)</div>', re.S|re.M)
     res = re.findall(reg1, r.text)
     if len(res) != 0:
         html = res[0]
-        reg2 = re.compile('<a.*?href="(.*?)".*?>\d+</a>',re.S|re.M)
+        reg2 = re.compile(r'<a.*?href="(.*?)".*?>\d+</a>', re.S|re.M)
         url_arr = re.findall(reg2, html)
         return url_arr
-    else:
-        return []
-
+    return []
 
 def findTitle(pages):
+    """find all title links"""
     t = []
     for page in pages:
         print('Connecting to "'+page+'":')
-        r = requests.get(page, headers = header)
-        if r.status_code==200:
+        r = requests.get(page, headers=header)
+        if r.status_code == 200:
             print('Success')
-            reg = re.compile('<td\s*class="title"\s*?>\s*?<a href="(.*?)".*?>.*?</a></td>',re.S|re.M)
+            reg = re.compile(r'<td\s*class="title"\s*?>\s*?<a href="(.*?)".*?>.*?</a></td>', re.S|re.M)
             titles = re.findall(reg, r.text)
             print(titles)
-            t+=titles
+            t += titles
         else:
             print('Error')
     return t
 
-
-
 def downloadImage(imgurls):
+    """download image"""
     for i in imgurls:
         print('Downloading '+i+':')
-        r = requests.get(i,stream = True)
-        if r.status_code==200:
+        r = requests.get(i, stream=True)
+        if r.status_code == 200:
             size = 0
-            path = '/cat/img/'+i.split('/')[-1]
-            with open(path,'wb') as f:
+            path = 'cat/img/'+i.split('/')[-1]
+            with open(path, 'wb') as f:
                 for chunk in r.iter_content(1024):
                     f.write(chunk)
-                    size+=1
+                    size += 1
             print('Success')
-            if size<50:
+            if size < 50:
                 os.remove(path)
                 print('Delete '+i)
         else:
             print('Error')
 
 
+def main():
+    """main function"""
+    url = 'https://www.douban.com/group/417139/discussion'
+    r = requests.get(url, headers=header)
+    if r.status_code != 200:
+        print(r.status_code)
+        exit()
+    print('Page_links:')
+    pages = getPage(r)
+    print(pages)
+    print('Title_url:')
+    titles_url = findTitle(pages)
+    print(titles_url)
+    for title_url in titles_url:
+        print('Connecting to "'+title_url+'":')
+        r = requests.get(title_url, headers=header)
+        if r.status_code == 200:
+            print('Success')
+            reg1 = re.compile('<body>(.*?)</body>', re.M|re.S)
+            body = re.findall(reg1, r.text)[0]
+            reg2 = re.compile('<div.*?class="topic-figure cc">.*?<img.*?src="(.*?)".*?>.*?</div>', re.M|re.S)
+            img_arr = re.findall(reg2, body)
+            new_arr = []
+            for i in img_arr:
+                a = re.match(r'[a-zA-z]+://[^\s]*', i)
+                if a is not None:
+                    new_arr.append(i)
+            print(new_arr)
+            downloadImage(new_arr)
+        else:
+            print('Error')
+    print('All titles: ' + len(titles_url))
+main()
 
-url = 'https://www.douban.com/group/417139/discussion'
-r = requests.get(url,headers = header)
-if r.status_code!=200:
-    print(r.status_code)
-    exit()
-print('Page_links:')
-pages = getPage(r)
-print(pages)
-print('Title_url:')
-titles_url = findTitle(pages)
-print(titles_url)
-for title_url in titles_url[:30]:
-    print('Connecting to "'+title_url+'":')
-    r = requests.get(title_url,headers = header)
-    if r.status_code==200:
-        print('Success')
-        reg = re.compile('<img src="(.*?)"')
-        img_arr = re.findall(reg, r.text)
-        new_arr = []
-        for i in img_arr:
-            a = re.match(r'[a-zA-z]+://[^\s]*',i)
-            if a is not None:
-                new_arr.append(i)
-        print(new_arr)
-        downloadImage(new_arr)
-    else:
-        print('Error')
+
